e90793b0bd44471f9ee96dbf9b481f1df2aff4d8
shinchain102 <79735602+shinchain102@users.noreply.github.com>
2025-02-18 12:16:40 +0530

    Updated Font to Ivyora

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ivyora: ['ivyora', 'serif'], // Define Ivyora as a custom font
+      },
+    },
   },
   plugins: [],
 };
